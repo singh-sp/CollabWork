73142d24f698c742a1e641d0be804f356bf350a1
Sant <spsingh113@gmail.com>
2017-02-25 14:29:23 -0800

    - Work in progress (main view)…

--- Swift/VendingMachine/VendingMachine/ViewController.swift
+++ Swift/VendingMachine/VendingMachine/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTapped(gesture:)))
+        
+        // add it to the image view;
+        VendItem1.addGestureRecognizer(tapGesture)
+        // make sure imageView can be interacted with by user
+        VendItem1.isUserInteractionEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var VendItem1: UIImageView!
+    @IBOutlet weak var VendItem2: UIImageView!
+    
+    @IBOutlet weak var CurrentPriceLabel: UILabel!
+    
+    func imageTapped(gesture: UIGestureRecognizer) {
+        // if the tapped view is a UIImageView then set it to imageview
+        if (gesture.view as? UIImageView) != nil {
+            CurrentPriceLabel.text = "Rs. 10"
+            //Here you can initiate your new ViewController
+            
+        }
+    }
 }
 
